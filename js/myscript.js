4e09680c1fd06157bdca417ae53ad46e4731b956
CuonoMaione <cuonomaione99@outlook.it>
2023-05-12 15:33:08 +0200

    add script

---
+++ js/myscript.js
@@ -0,0 +1,21 @@
+
+const userAge = parseInt(prompt ('insert your Age') ) ;
+
+const userKm = parseInt(prompt("Insert distance")) ;
+
+let Price = (userKm * 0.21 );
+
+console.log(Price)
+
+let result;
+
+if(userAge <= 17 ){
+    result = parseFloat(Price - [(Price * 20) /100]).toFixed(2) ;
+} else if (userAge > 65) {
+    result = parseFloat(Price - [(Price * 40) /100]).toFixed(2) ;
+} else {
+    result = parseFloat(Price).toFixed(2) ;
+};
+
+
+document.getElementById("chilometri").innerHTML += result + "£";
